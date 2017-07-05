c6025109b47c6b7ec06792bdc3527d4807079080
btsomogyi <blue.somogyi@gmail.com>
2017-07-05 12:28:30 -0400

    function model

--- k8lb/k8lb.sh
+++ k8lb/k8lb.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-(
 set -x
 
-k8lb_install() {
+install() {
 
   # Install and configure HAProxy
   cd ${cliqrAppTierName}/haproxy
@@ -30,56 +29,3 @@ EOF
 
   cp ${WD}/token.csv ~/.
 }
-
-# Action selection
-local CMD=$1
-case $CMD in
-	install)
-		log "[INSTALL] Installing $cliqrAppTierName"
-		k8lb_install
-		;;
-	deploy)
-		;;
-	configure)
-		log "[CONFIGURE] Configuring $cliqrAppTierName"
-
-		;;
-	start)
-	 	log "[START] Mounting storage volumes"
-
-		log "[START] Invoking pre-start user script"
-
-		log "[START] Starting $cliqrAppTierName"
-
-		log "[START] Invoking post-start user script"
-
-    	log "[START] $cliqrAppTierName successfully started."
-		;;
-	stop)
-		log "[STOP] Invoking pre-stop user script"
-
-		log "[STOP] Stopping $cliqrAppTierName"
-
-		log "[STOP] Invoking post-stop user script"
-
-		log "[STOP] $cliqrAppTierName successfully stopped."
-		;;
-	restart)
-		log "[RESTART k8lb] Invoking pre-restart user script"
-		;;
-	reload)
-		log "[RELOAD k8lb] Invoking pre-reload user script"
-		;;
-	cleanup)
-
-        ;;
-    upgrade)
-
-        ;;
-	*)
-		log "[ERROR] unknown command"
-		exit 127
-		;;
-esac
-
-)>> /var/tmp/master.log 2>&1
--- profile.sh
+++ profile.sh
@@ -5,18 +5,15 @@ set -x
 
 source /usr/local/osmosix/etc/userenv
 source /usr/local/osmosix/etc/.osmosix.sh
-export
 
 BASE_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export BASE_DIR
 
 source ${BASE_DIR}/util/function.sh
 USE_PROFILE_LOG == "true"
-export
 
 prepareEnvironment
 
-local CMD=$1
+CMD=$1
 TIER=${cliqrAppTierName}
 export
 
@@ -29,5 +26,61 @@ else
   log "[$CMD] Error: ${BASE_DIR}/${TIER}/${TIER}.sh not found"
 fi
 
+# Action selection
+local CMD=$1
+case $CMD in
+	install)
+    if [ -f ${BASE_DIR}/${TIER}/${TIER}.sh ]; then
+      source ${BASE_DIR}/${TIER}/${TIER}.sh
+      log "[INSTALL ${TIER}] Installing ${TIER}"
+      ${BASE_DIR}/${TIER}/${TIER}.sh install
+    else
+      log "[INSTALL ${TIER}] Error: ${BASE_DIR}/${TIER}/${TIER}.sh not found"
+      exit 127
+    fi
+		;;
+	deploy)
+		;;
+	configure)
+		log "[CONFIGURE ${TIER}] Configuring $cliqrAppTierName"
+
+		;;
+	start)
+	 	log "[START ${TIER}] Mounting storage volumes"
+
+		log "[START ${TIER}] Invoking pre-start user script"
+
+		log "[START ${TIER}] Starting ${TIER}"
+
+		log "[START ${TIER}] Invoking post-start user script"
+
+    log "[START ${TIER}] ${TIER} successfully started."
+		;;
+	stop)
+		log "[STOP ${TIER}] Invoking pre-stop user script"
+
+		log "[STOP ${TIER}] Stopping $cliqrAppTierName"
+
+		log "[STOP ${TIER}] Invoking post-stop user script"
+
+		log "[STOP ${TIER}] $cliqrAppTierName successfully stopped."
+		;;
+	restart)
+		log "[RESTART ${TIER}] Invoking pre-restart user script"
+		;;
+	reload)
+		log "[RELOAD ${TIER}] Invoking pre-reload user script"
+		;;
+	cleanup)
+
+      ;;
+  upgrade)
+
+      ;;
+	*)
+		log "[ERROR] unknown command"
+		exit 127
+		;;
+esac
 
 )>> /var/tmp/master.log 2>&1
